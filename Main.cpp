f8166546cf7e3a3a940d60cc2c5d677175e08698
sukim062534 <sukim062534@gmail.com>
2026-03-12 13:54:37 +0900

    Game

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-
+void GameMap(int Game[10][10], int& PlayerX, int& PlayerY);
+void GameKey(int Game[10][10], int& PlayerX, int& PlayerY);
 
 int main()
 {
@@ -25,63 +26,69 @@ int main()
 	int PlayerY = 1;
 
 	for (;;)
-	{
-		for (int Y = 0; Y < 10; ++Y)
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-
-				if (PlayerX == X && PlayerY == Y)
-				{
-					cout << "P";
-				}
-				else if (Game[Y][X] == 1)
-				{
-					cout << "#";
-
-				}
-				else if (Game[Y][X] == 0)
-				{
-					cout << " ";
-				}
-
-			}
-			cout << endl;
-		}
-	
-		int Key;
-		Key = _getch();
-		
-		if (Key == 'w')
-		{
-			if (Game[PlayerY - 1][PlayerX] == 0)
-			{
-				PlayerY--;
-			}
-		}
-		if (Key == 'a')
-		{
-			if (Game[PlayerY][PlayerX - 1] == 0)
-			{
-				PlayerX--;
-			}
-		}
-		if (Key == 's')
-		{
-			if (Game[PlayerY+1][PlayerX] == 0)
-			{
-				PlayerY++;
-			}
-		}
-		if (Key == 'd')
-		{
-			if (Game[PlayerY][PlayerX +1] == 0)
-			{
-				PlayerX++;
-			}
-		}
-
+	{	
 		system("cls");
+		GameMap(Game,PlayerX, PlayerY);
+		GameKey(Game, PlayerX, PlayerY);
 	}
 	return 0;
+}
+void GameMap(int Game[10][10], int& PlayerX, int& PlayerY)
+{
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (Game[Y][X] == 1)
+			{
+				cout << "#";
+
+			}
+			else if (Game[Y][X] == 0)
+			{
+				cout << " ";
+			}
+
+		}
+		cout << endl;
+	}
+}
+void GameKey(int Game[10][10], int& PlayerX, int& PlayerY)
+{
+	int Key;
+	Key = _getch();
+
+	if (Key == 'w')
+	{
+		if (Game[PlayerY - 1][PlayerX] == 0)
+		{
+			PlayerY--;
+		}
+	}
+	if (Key == 'a')
+	{
+		if (Game[PlayerY][PlayerX - 1] == 0)
+		{
+			PlayerX--;
+		}
+	}
+	if (Key == 's')
+	{
+		if (Game[PlayerY + 1][PlayerX] == 0)
+		{
+			PlayerY++;
+		}
+	}
+	if (Key == 'd')
+	{
+		if (Game[PlayerY][PlayerX + 1] == 0)
+		{
+			PlayerX++;
+		}
+	}
 }
